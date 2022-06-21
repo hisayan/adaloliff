c31c4821bca9fb72a91545bbca4b6bebe3a98973
hisayan <tom@anotherbrain.co.jp>
2022-06-21 09:56:50 +0900

    0.0.2 テスト前

--- src/components/Liff/index.js
+++ src/components/Liff/index.js
@@ -1,22 +1,46 @@
-import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { useEffect } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import liff from "@line/liff";
 
 const Liff = (props) => {
-	const { color, text } = props
+  console.log({ props });
+  const { liffId, userId, displayName } = props;
+  useEffect(() => {
+    liff
+      .init({ liffId })
+      .then(() => {
+        const isLoggedIn = liff.isLoggedIn();
+        liff
+          .getProfile()
+          .then((profile) => {
+            console.log("profile", profile);
+            userId.onChange(profile.userId);
+            displayName.onChange(profile.displayName);
+            // const name = profile.displayName
+            // const userId = profile.userId
+          })
+          .catch((err) => {
+            console.log("error2", err);
+          });
+      })
+      .catch((err) => {
+        console.log("error1", err);
+      });
+  }, []);
 
-	return(
-		<View style={styles.wrapper}>
-			<Text style={{ color }}>{text}</Text>
-		</View>
-	)
-}
+  return (
+    <View style={styles.wrapper}>
+      <Text style={{ color: "yellow" }}>{liffId}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-	wrapper: {
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-	}
-})
+  wrapper: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
-export default Liff
+export default Liff;
