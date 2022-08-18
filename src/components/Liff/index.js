4d43506549c88b8386b57b28a3a874dab38a995f
hisayan <tom@anotherbrain.co.jp>
2022-08-18 21:15:45 +0900

    green

--- src/components/Liff/index.js
+++ src/components/Liff/index.js
@@ -1,36 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import liff from "@line/liff";
 
 const Liff = (props) => {
-  console.log({ props });
+  // const [liffObject, setLiffObject] = useState;
+  // console.log({ props });
   const { liffId, userId, displayName } = props;
+  const [liffInitialized, setLiffInitialized] = useState(false);
   useEffect(() => {
-    liff
-      .init({ liffId })
-      .then(() => {
-        const isLoggedIn = liff.isLoggedIn();
-        liff
-          .getProfile()
-          .then((profile) => {
-            console.log("profile", profile);
-            userId.onChange(profile.userId);
-            displayName.onChange(profile.displayName);
-            // const name = profile.displayName
-            // const userId = profile.userId
-          })
-          .catch((err) => {
-            console.log("error2", err);
-          });
-      })
-      .catch((err) => {
-        console.log("error1", err);
-      });
+    // liff.ready.then(() => {
+    //   if (liff.isLoggedIn()) {
+    //     const context = liff.getContext();
+    //     const liffToken = liff.getAccessToken();
+    //     setUid(context.userId);
+    //     setAccessToken(liffToken);
+    //   }
+    // });
+
+    if (liffId && !liffInitialized && ! userId.value) {
+      console.log({ liffId, userId, displayName });
+      liff
+        .init({ liffId })
+        .then(() => {
+          setLiffInitialized(true);
+          const isLoggedIn = liff.isLoggedIn();
+          console.log({isLoggedIn})
+          if (isLoggedIn) {
+            liff
+              .getProfile()
+              .then((profile) => {
+                console.log("profile", profile);
+                if (profile.userId != userId.value) {
+                  userId.onChange(profile.userId);
+                  displayName.onChange(profile.displayName);
+                  liffInitialized.onChange(true);
+                }
+                // const name = profile.displayName
+                // const userId = profile.userId
+              })
+              .catch((err) => {
+                console.log("error2", err);
+              });
+          }
+        })
+        .catch((err) => {
+          console.log("error1", err);
+        });
+    }
   }, []);
 
   return (
     <View style={styles.wrapper}>
-      <Text style={{ color: "yellow" }}>{liffId}</Text>
+      <Text style={{ color: "green", display: props.editor ? "unset" : "none" }}>
+        {liffId}
+      </Text>
     </View>
   );
 };
